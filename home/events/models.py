7704bae73718185fcc996a601db35b68d612f5fc
kartik-thummar <kthummar786@gmail.com>
2021-04-16 16:46:21 +0530

    updated

--- home/events/models.py
+++ home/events/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
     sno = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=300)
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.CharField(max_length=15)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.CharField(max_length=50)
     slug = models.CharField(max_length=100)
     timeStamp = models.DateTimeField(blank=True)
 
+    # category = models.CharField(max_length=50)    
+
     def __str__(self):
-        return self.title + ' by ' + self.author
+        return self.title 
+        # + ' by ' + self.author
--- home/events/views.py
+++ home/events/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render,HttpResponse
 from events.models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
 def events(request):
     allPosts = Post.objects.all()
-    return render(request, 'events.html', {'allPosts': allPosts})
+    context = {'allPosts': allPosts}
+    return render(request, 'events.html', context)
+
+# class events(ListView):
+#     model = Post
+#     template_name = 'events.html'
 
 def eventPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
     context = {'post':post}
     return render(request, 'eventsPost.html', context)
 
+# def category_list(request):
+#     categories = Category.objects.all() 
+
+#     return render (request, 'blog/category_list.html', {'categories': categories}) 
+
+# def category_detail(request, pk):
+#     category = get_object_or_404(Category, pk=pk)
+
+#     return render(request, 'blog/category_detail.html', {'category': category}) 
+
+
 def about(request):
     return render(request, 'about.html')
 
-def search(request):
-    query = request.GET['query']
-    allPosts = Post.objects.filter(title__icontains=query)
-    params = {'allPosts': allPosts }
-    return render(request, 'search.html', params)
+# def search(request):
+#     query = request.GET['query']
+#     allPosts = Post.objects.filter(title__icontains=query)
+#     params = {'allPosts': allPosts }
+#     return render(request, 'search.html', params)
